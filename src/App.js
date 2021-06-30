252d2988f422da93a08ee23759c5ee27064b9fb8
Ashraf Azemi <ashraf.chzm@gmail.com>
2021-06-30 15:20:10 +0800

    useReducer practice

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useReducer } from "react";
+import "./App.css";
+import Modal from "./modal";
+
+const reducer = (state, action) => {
+  const newPeople = [...state.people, action.payload];
+  if (action.type === "NAME_ADDED") {
+    return {
+      ...state,
+      people: newPeople,
+      modalIsOpen: true,
+      modalContent: "item ADDED",
+    };
+  } else {
+    return {
+      ...state,
+      modalIsOpen: false,
+      modalContent: "Please enter name",
+    };
+  }
+};
+
+const defaultState = {
+  people: [],
+  modalIsOpen: false,
+  modalContent: "Please add name",
+};
 
 function App() {
+  const [name, setName] = useState("");
+  const [state, dispatch] = useReducer(reducer, defaultState);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newName = { id: new Date().getTime().toString(), name };
+    if (name) {
+      dispatch({ type: "NAME_ADDED", payload: newName });
+    } else {
+      dispatch({ type: "NO_INPUT" });
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Modal modalContent={state.modalContent} />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button type="submit">Add name</button>
+      </form>
+      {state.people.map((names) => {
+        const { name } = names;
+        return <h2>{name}</h2>;
+      })}
+    </>
   );
 }
 
---
+++ src/modal.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const modal = ({ modalContent }) => {
+  return (
+    <div>
+      <h2>{modalContent}</h2>
+    </div>
+  );
+};
+
+export default modal;
